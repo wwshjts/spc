666306ad0f17bad8b910aeae02afc84854f39874
Yury Kareba <wwsh@gmail.com>
2024-11-30 16:14:45 +0700

    Tests added

--- src/test/scala/parse/Atoms.scala
+++ src/test/scala/parse/Atoms.scala
@@ -67,14 +67,65 @@ class Atoms extends AnyFunSuite {
       )
 
     assertResult(expected)(res)
-
-
   }
+
   test("index expression") {
-    val input = Lexer("set[2]")
+    val input = Lexer("set[2][2 * n + 1]")
     val res = expression(input)
 
-    println(res)
+    val expected =
+      Success(
+        Index(
+          Index(
+            IdentifierName(IDENTIFIER(input(0))),
+            OPEN_BRACKET(input(1)),
+            IntegerLiteral(INTEGER(input(2))),
+            CLOSE_BRACKET(input(3))
+          ),
+          OPEN_BRACKET(input(4)),
+          ADD(
+            MULTIPLY(
+              IntegerLiteral(INTEGER(input(5))),
+              ASTERISK(input(6)),
+              IdentifierName(IDENTIFIER(input(7)))
+            ),
+            PLUS(input(8)),
+            IntegerLiteral(INTEGER(input(9)))
+          ),
+          CLOSE_BRACKET(input(10))
+        ),
+        List()
+      )
+
+    assertResult(expected)(res)
+
+  }
+
+  test("invoke") {
+    val input = Lexer("f(a, b, 3 + 1)")
+    val res = expression(input)
+
+    val expected =
+      Success(
+        Invoke(
+          IdentifierName(IDENTIFIER(input(0))),
+          OPEN_PAREN(input(1)),
+          SeparatedList(
+            IdentifierName(IDENTIFIER(input(2))),
+            COMMA(input(3)),
+            IdentifierName(IDENTIFIER(input(4))),
+            COMMA(input(5)),
+            ADD(
+              IntegerLiteral(INTEGER(input(6))),
+              PLUS(input(7)),
+              IntegerLiteral(INTEGER(input(8))))
+          ),
+          CLOSE_PAREN(input(9))
+      ), List()
+    )
+
+
+    assertResult(expected)(res)
   }
 
   test("tricky") {
