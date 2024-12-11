897cf68d3ff0dc8197f34d1a4e26ec7c6870b6c4
Yury Kareba <wwsh@gmail.com>
2024-12-11 21:35:09 +0700

    Work in progress

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,12 +2,14 @@ package org.syspro.spc
 
 import org.sypro.spc.lexer.SpcLexer
 import org.syspro.spc.parser.grammar.Grammar
-import syspro.tm.Tasks
+import syspro.tm.{Tasks, WebServer}
 import syspro.tm.lexer.TestMode
 
 @main
 def main(): Unit = {
   //Tasks.Lexer.registerSolution(new SpcLexer(),  new TestMode().strict(true))
+  WebServer.start()
   Tasks.Parser.registerSolution(Grammar)
+  WebServer.waitForWebServerExit()
 }
 
